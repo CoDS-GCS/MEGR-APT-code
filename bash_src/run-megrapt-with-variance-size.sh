0d7219c0ea276ef64f69fd2db094988008e0bcf3
Ahmed Aly <ahmed3amerai@gmail.com>
2024-01-05 14:49:55 -0500

    Vary size Constrain

--- bash_src/run-megrapt-with-variance-size.sh
+++ bash_src/run-megrapt-with-variance-size.sh
@@ -49,11 +49,9 @@ fi
 preprocess_graph () {
     QG=$1
     pg_name=$2
-    QG_folder=$3
-    QG_IOCs=$4
-    Max_Nodes_Mult=$5
-    Max_Edges_Mult=$6
-    output_prx=$7
+    output_prx=$3
+    Max_Nodes_Mult=$4
+    Max_Edges_Mult=$5
     echo ${output_prx}
     stardog-admin db online ${stardog_db} -u ahmed3amerai@gmail.com -p Stardog_Ahmed_2023
     if [ ! -f ./dataset/${dataset}/experiments/${output_prx}/raw/torch_prediction/${QG}_in_${pg_name}.pt ]; then
@@ -85,55 +83,54 @@ predict_model () {
     echo "Predicting PG ${pg_name} with model parameters ${layer}rgcn_Lr${LR}_Dr${DR}_${vector1}-${vector2}-${vector3}_${ep}"
     python -u ./src/main.py --dataset ${dataset} --dataset-path ./dataset/${dataset_name}/experiments/${output_prx}/ --gnn-operator rgcn --embedding-layers ${layer} --learning-rate ${LR} --dropout ${DR} --epochs ${ep} --filters-1 ${vector1} --filters-2 ${vector2} --filters-3 ${vector3} --tensor-neurons ${vector3} --predict --predict-folder raw/torch_prediction/ --log-similarity --threshold ${Threshold} --load ./model/megrapt/${dataset_name}/${dataset_name}_${layer}rgcn_Lr${LR}_Dr${DR}_${vector1}-${vector2}-${vector3}_${ep}.pt > logs/${dataset_name}/${output_prx}/Evaluate_Per_Host/${dataset_name}_${layer}rgcn_Lr${LR}_Dr${DR}_${vector1}-${vector2}-${vector3}_${ep}_TH${Threshold}_${output_prx}_${date}.txt
     python -u ./src/main.py --dataset ${dataset} --dataset-path ./dataset/${dataset_name}/experiments/${output_prx}/ --gnn-operator rgcn --embedding-layers ${layer} --learning-rate ${LR} --dropout ${DR} --epochs ${ep} --filters-1 ${vector1} --filters-2 ${vector2} --filters-3 ${vector3} --tensor-neurons ${vector3} --predict --predict-folder raw/torch_prediction/ --plot-thresholds --load ./model/megrapt/${dataset_name}/${dataset_name}_${layer}rgcn_Lr${LR}_Dr${DR}_${vector1}-${vector2}-${vector3}_${ep}.pt
-
 }
+
 run_megrapt () {
-  Max_Nodes_Mult=$1
-  Max_Edges_Mult=$2
-  output_prx=$3
+  output_prx=$1
+  Max_Nodes_Mult=$2
+  Max_Edges_Mult=$3
+  echo ${output_prx}
   Threshold=0.4
   if [[ "$host" == "cadets" ]]
   then
-      preprocess_graph BSD_1 attack_BSD_1 ${QG_folder} ${QG_IOCs} ${Max_Nodes_Mult} ${Max_Edges_Mult} ${output_prx}
-      preprocess_graph BSD_2 attack_BSD_2 ${QG_folder} ${QG_IOCs} ${Max_Nodes_Mult} ${Max_Edges_Mult} ${output_prx}
-      preprocess_graph BSD_3 attack_BSD_3_4 ${QG_folder} ${QG_IOCs} ${Max_Nodes_Mult} ${Max_Edges_Mult} ${output_prx}
-      preprocess_graph BSD_4 attack_BSD_3_4 ${QG_folder} ${QG_IOCs} ${Max_Nodes_Mult} ${Max_Edges_Mult} ${output_prx}
+      preprocess_graph BSD_1 attack_BSD_1 ${output_prx} ${Max_Nodes_Mult} ${Max_Edges_Mult}
+      preprocess_graph BSD_2 attack_BSD_2 ${output_prx} ${Max_Nodes_Mult} ${Max_Edges_Mult}
+      preprocess_graph BSD_3 attack_BSD_3_4 ${output_prx} ${Max_Nodes_Mult} ${Max_Edges_Mult}
+      preprocess_graph BSD_4 attack_BSD_3_4 ${output_prx} ${Max_Nodes_Mult} ${Max_Edges_Mult}
       for Query in {BSD_1,BSD_2,BSD_3,BSD_4}; do
-      preprocess_graph ${Query} benign_BSD ${QG_folder} ${QG_IOCs} ${Max_Nodes_Mult} ${Max_Edges_Mult} ${output_prx}
+      preprocess_graph ${Query} benign_BSD ${output_prx} ${Max_Nodes_Mult} ${Max_Edges_Mult}
       done
   elif [[ "$host" == "theia" ]]
   then
-      preprocess_graph Linux_1 attack_linux_1_2 ${QG_folder} ${QG_IOCs} ${Max_Nodes_Mult} ${Max_Edges_Mult} ${output_prx}
-      preprocess_graph Linux_2 attack_linux_1_2 ${QG_folder} ${QG_IOCs} ${Max_Nodes_Mult} ${Max_Edges_Mult} ${output_prx}
+      preprocess_graph Linux_1 attack_linux_1_2 ${output_prx} ${Max_Nodes_Mult} ${Max_Edges_Mult}
+      preprocess_graph Linux_2 attack_linux_1_2 ${output_prx} ${Max_Nodes_Mult} ${Max_Edges_Mult}
       for Query in {Linux_1,Linux_2}; do
-          preprocess_graph ${Query} benign_theia ${QG_folder} ${QG_IOCs} ${Max_Nodes_Mult} ${Max_Edges_Mult} ${output_prx}
+          preprocess_graph ${Query} benign_theia ${output_prx} ${Max_Nodes_Mult} ${Max_Edges_Mult}
       done
   elif [[ "$host" == "trace" ]]
   then
-      preprocess_graph Linux_3 attack_linux_3 ${QG_folder} ${QG_IOCs} ${Max_Nodes_Mult} ${Max_Edges_Mult} ${output_prx}
-      preprocess_graph Linux_4 attack_linux_4 ${QG_folder} ${QG_IOCs} ${Max_Nodes_Mult} ${Max_Edges_Mult} ${output_prx}
+      preprocess_graph Linux_3 attack_linux_3 ${output_prx} ${Max_Nodes_Mult} ${Max_Edges_Mult}
+      preprocess_graph Linux_4 attack_linux_4 ${output_prx} ${Max_Nodes_Mult} ${Max_Edges_Mult}
       for Query in {Linux_3,Linux_4}; do
-          preprocess_graph ${Query} benign_trace ${QG_folder} ${QG_IOCs} ${Max_Nodes_Mult} ${Max_Edges_Mult} ${output_prx}
+          preprocess_graph ${Query} benign_trace ${output_prx} ${Max_Nodes_Mult} ${Max_Edges_Mult}
       done
   elif [[ "$host" == "optc" ]]
   then
       for PG in {benign_SysClient0201,benign_SysClient0501,benign_SysClient0051,benign_SysClient0358}; do
           for Query in {Plain_PowerShell_Empire,Custom_PowerShell_Empire,Malicious_Upgrade}; do
-              preprocess_graph ${Query} ${PG} ${QG_folder} ${QG_IOCs} ${Max_Nodes_Mult} ${Max_Edges_Mult} ${output_prx}
+              preprocess_graph ${Query} ${PG} ${output_prx} ${Max_Nodes_Mult} ${Max_Edges_Mult}
           done
       done
-      preprocess_graph Malicious_Upgrade attack_SysClient0051 ${QG_folder} ${QG_IOCs} ${Max_Nodes_Mult} ${Max_Edges_Mult} ${output_prx}
-      preprocess_graph Custom_PowerShell_Empire attack_SysClient0358 ${QG_folder} ${QG_IOCs} ${Max_Nodes_Mult} ${Max_Edges_Mult} ${output_prx}
-      preprocess_graph Custom_PowerShell_Empire attack_SysClient0501 ${QG_folder} ${QG_IOCs} ${Max_Nodes_Mult} ${Max_Edges_Mult} ${output_prx}
+      preprocess_graph Malicious_Upgrade attack_SysClient0051 ${output_prx} ${Max_Nodes_Mult} ${Max_Edges_Mult}
+      preprocess_graph Custom_PowerShell_Empire attack_SysClient0358 ${output_prx} ${Max_Nodes_Mult} ${Max_Edges_Mult}
+      preprocess_graph Custom_PowerShell_Empire attack_SysClient0501 ${output_prx} ${Max_Nodes_Mult} ${Max_Edges_Mult}
       sleep 300
-      preprocess_graph Plain_PowerShell_Empire attack_SysClient0201 ${QG_folder} ${QG_IOCs} ${Max_Nodes_Mult} ${Max_Edges_Mult} ${output_prx}
+      preprocess_graph Plain_PowerShell_Empire attack_SysClient0201 ${output_prx} ${Max_Nodes_Mult} ${Max_Edges_Mult}
   else
       echo "Undefined host."
   fi
-
   # Default Parameters
   predict_model 2 0.001 64 64 32 0 1000 ${Threshold} ${output_prx}
-
   if [[ "$host" == "cadets" ]]
   then
     predict_model 2 0.001 128 92 64 0 1000 ${Threshold} ${output_prx}
@@ -157,23 +154,23 @@ Max_Edges_Mult=25
 for Max_Nodes_Mult in {5,15,20};do
   output_prx="${output_prx_root}_${Max_Nodes_Mult}_Nodes_${Max_Edges_Mult}_Edges"
   echo "The output forlder is: ${output_prx}"
-#  run_megrapt ${Max_Nodes_Mult} ${Max_Edges_Mult} ${output_prx}
-#  sleep 300
+  run_megrapt ${output_prx} ${Max_Nodes_Mult} ${Max_Edges_Mult}
+  sleep 300
 done
 Max_Nodes_Mult=10
 for Max_Edges_Mult in {15,20,30};do
   output_prx="${output_prx_root}_${Max_Nodes_Mult}_Nodes_${Max_Edges_Mult}_Edges"
   echo "The output forlder is: ${output_prx}"
-#  run_megrapt ${Max_Nodes_Mult} ${Max_Edges_Mult} ${output_prx}
-#  sleep 300
+  run_megrapt ${output_prx} ${Max_Nodes_Mult} ${Max_Edges_Mult}
+  sleep 300
 done
 
 for Max_Nodes_Mult in {5,15,20};do
   for Max_Edges_Mult in {15,20,30};do
     output_prx="${output_prx_root}_${Max_Nodes_Mult}_Nodes_${Max_Edges_Mult}_Edges"
     echo "The output forlder is: ${output_prx}"
-#    run_megrapt ${Max_Nodes_Mult} ${Max_Edges_Mult} ${output_prx}
-#    sleep 300
+    run_megrapt ${output_prx} ${Max_Nodes_Mult} ${Max_Edges_Mult}
+    sleep 300
   done
 done
 
